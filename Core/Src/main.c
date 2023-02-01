b9f4e5f21ab9e30afa0782f8ba2cece6dedd83d9
Milos Dzaleta <milosdzaleta@live.com>
2023-02-01 22:22:58 +0100

    Initial configuration of I2C for OLED

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "dac.h"
 #include "dma.h"
+#include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -90,6 +91,7 @@ int main(void)
   MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_DAC1_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
