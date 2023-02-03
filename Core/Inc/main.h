b29ed2cc913037fad1d8cc3ea76cc126b696bb7d
Milos Dzaleta <milosdzaleta@live.com>
2023-02-03 15:37:24 +0100

    Include ssd1306 to main

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,6 +31,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "ssd1306_conf.h"
 
 /* USER CODE END Includes */
 
