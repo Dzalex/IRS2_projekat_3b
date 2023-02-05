33603c612266413b05f197a6b4dbe2d3b16a0f1d
Milos Dzaleta <milosdzaleta@live.com>
2023-02-05 19:06:00 +0100

    Create initial graphics files

---
+++ Core/Inc/graphics.h
@@ -0,0 +1,13 @@
+/*
+ * graphics.h
+ *
+ *  Created on: Feb 5, 2023
+ *      Author: milos
+ */
+
+#ifndef INC_GRAPHICS_H_
+#define INC_GRAPHICS_H_
+
+void Init_Graphics();
+
+#endif /* INC_GRAPHICS_H_ */
---
+++ Core/Src/graphics.c
@@ -0,0 +1,10 @@
+/*
+ * graphics.c
+ *
+ *  Created on: Feb 5, 2023
+ *      Author: milos
+ */
+
+#include "graphics.h"
+#include "main.h"
+
